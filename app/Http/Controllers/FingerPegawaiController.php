b9a436b0c814d797eac6e85985c56c53088b10af
WisnuPrayogo096 <kusumoprayogo123456@gmail.com>
2025-06-28 19:19:50 +0700

    add data items

--- app/Http/Controllers/FingerPegawaiController.php
+++ app/Http/Controllers/FingerPegawaiController.php
@@ -16,10 +16,10 @@ class FingerPegawaiController extends Controller
     {
         $user = Auth::user();
 
-        $validator = Validator::make($request->all(), [
-            'bulan' => 'nullable|integer|min:1|max:12',
-            'tahun' => 'nullable|integer|min:1900',
-            'page' => 'nullable|integer|min:1',
+        $validator = Validator::make($request->query(), [
+            'bulan' => 'nullable|numeric|min:1|max:12',
+            'tahun' => 'nullable|numeric|min:1900',
+            'page' => 'nullable|numeric|min:1',
         ]);
 
         if ($validator->fails()) {
@@ -29,10 +29,14 @@ class FingerPegawaiController extends Controller
             );
         }
 
-        $bulan = $request->input('bulan', Carbon::now()->month);
-        $tahun = $request->input('tahun', Carbon::now()->year);
+        $bulanQuery = $request->query('bulan');
+        $tahunQuery = $request->query('tahun');
+        $pageQuery = $request->query('page');
+
+        $bulan = $bulanQuery !== null ? (int)$bulanQuery : Carbon::now()->month;
+        $tahun = $tahunQuery !== null ? (int)$tahunQuery : Carbon::now()->year;
         $perPage = 10;
-        $page = $request->input('page', 1);
+        $page = $pageQuery !== null ? (int)$pageQuery : 1;
 
         if (!$user || empty($user->idf)) {
             throw new ResponseException(
@@ -44,8 +48,8 @@ class FingerPegawaiController extends Controller
         $idf = $user->idf;
 
         $fingerPegawai = FingerPegawai::where('id_finger', $idf)
-            ->whereYear('tanggal_absen', $tahun)
-            ->whereMonth('tanggal_absen', $bulan)
+            ->whereRaw('YEAR(tanggal_absen) = ?', [$tahun])
+            ->whereRaw('MONTH(tanggal_absen) = ?', [$bulan])
             ->orderByDesc('tanggal_absen')
             ->paginate($perPage, ['*'], 'page', $page);
 
@@ -56,6 +60,22 @@ class FingerPegawaiController extends Controller
             );
         }
 
-        return new BaseResponse($fingerPegawai->toArray(), 200);
+        $responseData = [
+            'current_page' => $fingerPegawai->currentPage(),
+            'items' => $fingerPegawai->items(),
+            'per_page' => $fingerPegawai->perPage(),
+            'total' => $fingerPegawai->total(),
+            'last_page' => $fingerPegawai->lastPage(),
+            'from' => $fingerPegawai->firstItem(),
+            'to' => $fingerPegawai->lastItem(),
+            'links' => [
+                'first' => $fingerPegawai->url(1),
+                'last' => $fingerPegawai->url($fingerPegawai->lastPage()),
+                'prev' => $fingerPegawai->previousPageUrl(),
+                'next' => $fingerPegawai->nextPageUrl(),
+            ]
+        ];
+
+        return new BaseResponse($responseData, 200);
     }
 }
