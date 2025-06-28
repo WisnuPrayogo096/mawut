44e04af61732ad4d988b97a0c5a4a0c6eb8f11d2
WisnuPrayogo096 <kusumoprayogo123456@gmail.com>
2025-06-28 17:02:04 +0700

    mawut bersatoe

--- app/Http/Middleware/VerifyToken.php
+++ app/Http/Middleware/VerifyToken.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Middleware;
 
-use App\Exceptions\ResponseException;
+use App\Http\Exceptions\ResponseException;
 use Closure;
 use Illuminate\Http\Request;
 use Laravel\Sanctum\PersonalAccessToken;
